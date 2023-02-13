66ded48d2af5107bb427ba72f06d80917caa62df
imranhossainemi <blur-imran@hotmail.com>
2023-02-13 20:00:09 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const author = document.querySelector('#author');
 const addBtn = document.querySelector('.btn-add');
 
 form.addEventListener('submit', () => {
-    const newBook = {
-      title: title.value,
-      author: author.value,
-    };
-  
-    // const obj = JSON.parse(localStorage.getItem('books'));
-  
-    // obj.allbook.push(newBook);
-  
-    // booklist = obj.allbook;
-    localStorage.setItem('books', JSON.stringify(newBook));
-  });
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+
+  // const obj = JSON.parse(localStorage.getItem('books'));
+
+  // obj.allbook.push(newBook);
+
+  // booklist = obj.allbook;
+  localStorage.setItem('books', JSON.stringify(newBook));
+});
