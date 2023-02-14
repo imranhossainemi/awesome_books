7407d444a3b1b38f5f2b1e52f26f4c6f91f16a04
imranhossainemi <blur-imran@hotmail.com>
2023-02-14 13:05:19 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 const form = document.querySelector('.form');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
-const addBtn = document.querySelector('.btn-add');
+const bookList = document.querySelector('.book-list');
+let bookArr = [];
+let book = '';
+const localItems = JSON.parse(localStorage.getItem('book'));
+
+if (localItems === null) {
+  bookArr = [];
+} else {
+  bookArr = JSON.parse(localStorage.getItem('book'));
+}
 
 form.addEventListener('submit', () => {
-  const newBook = {
-    title: title.value,
-    author: author.value,
-  };
-
-  // const obj = JSON.parse(localStorage.getItem('books'));
-
-  // obj.allbook.push(newBook);
-
-  // booklist = obj.allbook;
-  localStorage.setItem('books', JSON.stringify(newBook));
+  const newBook = { title: title.value, author: author.value };
+  bookArr.push(newBook);
+  localStorage.setItem('book', JSON.stringify(bookArr));
 });
+bookArr.forEach((item, index) => {
+  book += `
+  <div class='book'>
+    <p> "${item.title}" by ${item.author}</p>
+    <button type="button" class="remove-btn" id="${index}">Remove</button>
+  </div>`;
+});
+
+bookList.innerHTML = book;
+
+const removeBtn = document.querySelectorAll('.remove-btn');
+
+removeBtn.forEach((item) => item.addEventListener('click', () => {
+  const removeId = parseInt(item.id, 10);
+  let obj = JSON.parse(localStorage.getItem('book'));
+  bookArr = obj;
+  bookArr = bookArr.filter((element, index) => index !== removeId);
+  obj = bookArr;
+  localStorage.setItem('book', JSON.stringify(obj));
+  window.location.reload();
+}));
